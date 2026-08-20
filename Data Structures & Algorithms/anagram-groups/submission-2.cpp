5cb343d9a13a7af6ac2d7d9934387e9bbf321a7b
liberal-henrique <51170975+liberal-henrique@users.noreply.github.com>
2026-08-20 19:28:46 +0100

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        map<vector<int>, int> keyAndPosition;
+        for(string elem : strs) {
+            vector<int> key(26, 0);
+            for(char e: elem)
+                key[int(e) - 'a'] += 1;
+            auto it = keyAndPosition.find(key);
+            if (it != keyAndPosition.end())
+                result[it->second].push_back(elem);
+            else {
+                result.push_back({elem});
+                keyAndPosition.insert({key, result.size() - 1});
+            }
+        }
+        return result;
+    }
+};
